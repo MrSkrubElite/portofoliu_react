98c0a50e50d5f8127ab6a128e33fa03aa41dbb92
Stefanescu Florian-Alexandru <alexstefanescu128@yahoo.com>
2023-03-22 16:57:48 +0200

    +-progress

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,11 @@
 import './Navbar.css';
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckSquare, faCoffee, faMapLocation, faEnvelopeOpenText, faBars } from '@fortawesome/free-solid-svg-icons' 
 
 const Navbar = () => {
   return (
+    <>
     <nav className="nav-container">
         
             <div className="logo">
@@ -14,15 +17,32 @@ const Navbar = () => {
                 <a href="#projects"><h3>Projects</h3></a>
                 <a href="#contactme"><h3>Contact</h3></a>
             </div>
-            <div className="menu none">
-                <h3>Home</h3>
-                <h3>About</h3>
-                <h3>Projects</h3>
-                <h3>Contact</h3>
+            <div className="menu" onClick={menuBtn}>
+              <FontAwesomeIcon icon={faBars} />
             </div>
-        
     </nav>
+
+    <div id="menu-container" className="menuoff">
+        <a href="#hero" className='aList'><h3>Home</h3></a>
+        <a href="#aboutme" className='aList'><h3>About</h3></a>
+        <a href="#projects" className='aList'><h3>Projects</h3></a>
+        <a href="#contactme" className='aList'><h3>Contact</h3></a>
+      </div>
+    
+    </>
   );
+  
+ 
 }
+
+let menuContainer = document.getElementById("menu-container");
+
+
+let menuBtn = () => {
+  console.log("sofutpemata");
+  menuContainer.classList.remove("menuoff");
+}
+  
+
 
 export default Navbar;
